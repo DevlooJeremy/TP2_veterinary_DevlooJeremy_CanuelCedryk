1f447bb6b5f2f5436ec352b89ea18d1cca4bc027
Cedryk Canuel <cedrykcanuel@outlook.fr>
2023-12-18 12:17:38 -0500

    Partie A5 finie

--- src/dogs/view/DogListView.java
+++ src/dogs/view/DogListView.java
@@ -26,6 +26,9 @@ public class DogListView extends JFrame implements IView, ActionListener{
 	private static final String ID_TITLE = "ID";
 	private static final String NAME_TITLE = "NOM";
 	private static final String BREED_TITLE = "RACE";
+	private static final String NAME_CUSTOMER_TITLE = "NOM DU PROPRIÉTAIRE";
+	private static final String LAST_NAME_CUSTOMER_TITLE = "NOM DE FAMILLE DU PROPRIÉTAIRE";
+	private static final String PHONE_CUSTOMER_TITLE = "TÉLÉPHONE DU PROPRIETAIRE";
 	private static final String CLOSE_BTN_TXT = "OK";
 	private static final String CLOSE_BTN = "close_frame";
 
@@ -71,12 +74,12 @@ public class DogListView extends JFrame implements IView, ActionListener{
 		// container interm�diaire JPanel qui contient les �tiquettes (JLabel) et les zones de textes (JTextField)
 		// utiliser un GridLayout comme LayoutManager
 		JPanel createDogPanel = new JPanel();
-		createDogPanel.setLayout(new GridLayout(0,3));
+		createDogPanel.setLayout(new GridLayout(0,6));
 		this.add(createDogPanel, null);
 		this.addTitleTextField(createDogPanel);
 		Collection<Dog> dogList = this.repository.getList();
 		for (Dog dog : dogList) {
-			addTextField(createDogPanel,dog.getId(),dog.getName(),dog.getBreed());
+			addTextField(createDogPanel,dog.getId(),dog.getName(),dog.getBreed(), dog.getOwner().getName(), dog.getOwner().getLastName(), dog.getOwner().getPhone());
 		}
 		
 	}
@@ -85,13 +88,19 @@ public class DogListView extends JFrame implements IView, ActionListener{
 		panel.add(new JLabel(ID_TITLE));
 		panel.add(new JLabel(NAME_TITLE));
 		panel.add(new JLabel(BREED_TITLE));
+		panel.add(new JLabel(NAME_CUSTOMER_TITLE));
+		panel.add(new JLabel(LAST_NAME_CUSTOMER_TITLE));
+		panel.add(new JLabel(PHONE_CUSTOMER_TITLE));
 	}
 	
-	private void addTextField(JPanel panel, int id, String name, String breed) {
+	private void addTextField(JPanel panel, int id, String name, String breed, String nameCustomer, String lastNameCustomer, String phoneNumber) {
 		
 		panel.add(new JLabel(Integer.toString(id))); 
 		panel.add(new JLabel(name));
 		panel.add(new JLabel(breed));
+		panel.add(new JLabel(nameCustomer)); 
+		panel.add(new JLabel(lastNameCustomer));
+		panel.add(new JLabel(phoneNumber));
 
 	}
 
